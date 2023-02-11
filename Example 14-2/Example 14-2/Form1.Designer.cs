a47bea3bd38d2b303cb3598c26a55108b2c3a141
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 18:35:03 +0330

    Add files via upload

---
+++ Example 14-2/Example 14-2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Example_14_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(183, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(267, 34);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(36, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(77, 183);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(183, 139);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(258, 183);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(284, 116);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Example 14-2 (ContextMenuStrip)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Example 14-2/Example 14-2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_14_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //--------------------------------------------------------------------
+        ContextMenuStrip contextmenustrip1 = new ContextMenuStrip();
+        ToolStripMenuItem Cut_tsmi = new ToolStripMenuItem("&Cut");
+        ToolStripMenuItem Copy_tsmi = new ToolStripMenuItem("C&opy");
+        ToolStripMenuItem Paste_tsmi = new ToolStripMenuItem("&Paste");
+        ToolStripMenuItem Delete_tsmi = new ToolStripMenuItem("&Delete");
+        //--------------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            contextmenustrip1.Items.AddRange(new ToolStripItem[] {Cut_tsmi,Copy_tsmi,Paste_tsmi,Delete_tsmi});
+            Cut_tsmi.Click += new System.EventHandler(this.Cut_Copy_Paste_Delete_Click);
+            Copy_tsmi.Click += new System.EventHandler(this.Cut_Copy_Paste_Delete_Click);
+            Paste_tsmi.Click += new System.EventHandler(this.Cut_Copy_Paste_Delete_Click);
+            Delete_tsmi.Click += new System.EventHandler(this.Cut_Copy_Paste_Delete_Click);
+            this.ContextMenuStrip = contextmenustrip1;
+            foreach (Button btn in this.Controls)
+            {
+                btn.ContextMenuStrip = contextmenustrip1;
+            }
+        }
+        //--------------------------------------------------------------------
+        void GetProperties(ref Button destbtn, Button sourcebtn)
+        {
+            destbtn.Text = sourcebtn.Text;
+            destbtn.Size = sourcebtn.Size;
+            destbtn.Font=sourcebtn.Font;
+            destbtn.Location = sourcebtn.Location;
+            //,.....
+        }
+        //--------------------------------------------------------------------
+        private void Cut_Copy_Paste_Delete_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem tsmi = (ToolStripMenuItem)sender;
+            switch (tsmi.Text)
+            {
+                case "&Cut":
+                    if (contextmenustrip1.SourceControl != this)
+                    {
+                        Button b = new Button();
+                        GetProperties(ref b, (Button)contextmenustrip1.SourceControl);
+                        this.Tag = b;
+                        contextmenustrip1.SourceControl.Dispose();
+                    }
+                    break;
+                case "C&opy":
+                    if (contextmenustrip1.SourceControl != this)
+                    {
+                        Button b = new Button();
+                        GetProperties(ref b, (Button)contextmenustrip1.SourceControl);
+                        this.Tag = b;
+                    }
+                    break;
+                case "&Paste":
+                    if (this.Tag != null)
+                    {
+                        Button b1 = new Button();
+                        GetProperties(ref b1, (Button)this.Tag);
+                        b1.Location=new Point(MousePosition.X - this.Left-24 ,MousePosition.Y - this.Top-82);
+                        b1.ContextMenuStrip = contextmenustrip1;
+                        this.Controls.Add(b1);
+                        b1.Focus();
+                        b1.BringToFront();
+                    }
+                    break;
+                case "&Delete":
+                    if (contextmenustrip1.SourceControl != this)
+                    {
+                        contextmenustrip1.SourceControl.Dispose();
+                    }
+                    break;
+            }
+        }
+        //--------------------------------------------------------------------
+    }
+}
